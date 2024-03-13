6b556b602cf8effc07256b0b8560fb519f6ab522
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-03-12 20:23:39 -0400

    Update green.php

--- green.php
+++ green.php
@@ -5,17 +5,17 @@
 		<title>Green Blocks | COMP1002 HTML, CSS, and JS Fundamentals</title>
 		<meta name="author" content="The name of the document’s author to go here">
 		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 	<body id="section3">
 		<!-- Page-level header -->
 		<header>
-			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<img src="html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 <?php
-	include('includes/global-nav.php');
+	include('global-nav.php');
 ?>
 		</header>
 		<!-- Page-level main content -->
@@ -27,7 +27,7 @@
 			</section>
 			<aside>
 				<figure>
-					<img src="images/green-block.png" width="350" height="237" alt="Green block" />
+					<img src="green-block.png" width="350" height="237" alt="Green block" />
 					<figcaption><a href="https://openclipart.org/detail/224274/clip-is-a-brick-2x4" title="View the original artwork" rel="external">Brick</a> by Vanja (Public Domain)</figcaption>
 				</figure>
 			</aside>
@@ -35,7 +35,7 @@
 		<!-- Page-level footer -->
 		<footer>
 <?php
-	include('includes/footer-nav.php');
+	include('footer-nav.php');
 ?>
 			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
 		</footer>
