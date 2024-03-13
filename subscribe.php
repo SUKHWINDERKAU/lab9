1ea99369a5d410efca437da8900308668ba206b7
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-03-12 20:33:47 -0400

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -5,17 +5,17 @@
 		<title>Subscribe | COMP1002 HTML, CSS, and JS Fundamentals</title>
 		<meta name="author" content="The name of the document’s author to go here" />
 		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 	<body>
 		<!-- Page-level header -->
 		<header>
-			<img src="images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<img src="html-logo.svg" height="75" width="75" alt="HTML" />
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 <?php
-	include('includes/global-nav.php');
+	include('global-nav.php');
 ?>
 		</header>
 		<!-- Page-level main content -->
@@ -33,7 +33,7 @@
 		<!-- Page-level footer -->
 		<footer>
 <?php
-	include('includes/footer-nav.php');
+	include('footer-nav.php');
 ?>
 			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
 		</footer>
